884d62f41779fb8b8c83b4f4892fa8aee8861cea
aris-gk3 <arisilias3@gmail.com>
2024-03-28 11:56:34 +0200

    Test 2 -> Tof%Pof=1

--- header.h
+++ header.h
@@ -12,10 +12,10 @@
 #define NIF 6
 #define NIX 22
 #define NIY 22
-#define NOF 12
+#define NOF 20
 #define NOX ((NIX-NKX)/S + 1) // 10
 #define NOY ((NIY-NKY)/S + 1) // 10
-#define TOF 4
+#define TOF 10
 #define TOX NOX // 10
 #define TOY 10
 #define TIF NIF
